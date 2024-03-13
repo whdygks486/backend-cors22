667a86efaf1c0237b5107891f840d231d5eb9565
조요한 <whdygks486@naver.com>
2024-03-14 00:10:40 +0900

    11

--- mandarat-main/mandarat-main/src/main/java/com/swig/manda/config/SecurityConfig.java
+++ mandarat-main/mandarat-main/src/main/java/com/swig/manda/config/SecurityConfig.java
@@ -120,7 +120,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+       // configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
